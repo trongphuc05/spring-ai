aff3384ac38aa44e33bd275ab053fb0cd8f56584
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-24 10:26:46 +0700

    Add prompts

--- src/main/java/com/trongphuc05/spring_ai/service/ChatService.java
+++ src/main/java/com/trongphuc05/spring_ai/service/ChatService.java
@@ -2,6 +2,9 @@ package com.trongphuc05.spring_ai.service;
 
 import com.trongphuc05.spring_ai.dto.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +16,15 @@ public class ChatService {
     }
 
     public String chat(ChatRequest request) {
+        SystemMessage systemMessage = new SystemMessage("""
+                You are ai for Tpzz
+                Response super funny voice""");
+
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
         return chatClient
-                .prompt(request.message())
+                .prompt(prompt)
                 .call()
                 .content();
     }
