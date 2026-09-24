5bfe44f4e27d41e44e5f1b09af502c7d428b45b1
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-24 11:46:41 +0700

    process image

--- src/main/java/com/trongphuc05/spring_ai/controller/ChatController.java
+++ src/main/java/com/trongphuc05/spring_ai/controller/ChatController.java
@@ -4,7 +4,9 @@ import com.trongphuc05.spring_ai.dto.ChatRequest;
 import com.trongphuc05.spring_ai.service.ChatService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ChatController {
@@ -17,5 +19,11 @@ public class ChatController {
     @PostMapping("/chat")
     String chat(@RequestBody ChatRequest request) {
         return chatService.chat(request);
+    }
+
+    @PostMapping("/chat-with-image")
+    String chatWithImage(@RequestParam("file") MultipartFile file,
+                         @RequestParam("message") String message) {
+        return chatService.chatWithImage(file, message);
     }
 }
--- src/main/java/com/trongphuc05/spring_ai/service/ChatService.java
+++ src/main/java/com/trongphuc05/spring_ai/service/ChatService.java
@@ -4,8 +4,12 @@ import com.trongphuc05.spring_ai.dto.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ChatService {
@@ -15,12 +19,34 @@ public class ChatService {
         chatClient = builder.build();
     }
 
+    public String chatWithImage(MultipartFile file, String message) {
+        Media media = Media.builder()
+                .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+                .data(file.getResource())
+                .build();
+
+        ChatOptions chatOptions = ChatOptions.builder()
+                .temperature(0D)
+                .build();
+
+        return chatClient.prompt()
+                .options(chatOptions)
+                .system("You are Devteria.AI")
+                .user(promptUserSpec
+                        -> promptUserSpec.media(media)
+                        .text(message))
+                .call()
+                .content();
+    }
+
     public String chat(ChatRequest request) {
         SystemMessage systemMessage = new SystemMessage("""
-                You are ai for Tpzz
-                Response super funny voice""");
+                You are Devteria.AI
+                You should response with a formal voice
+                """);
 
         UserMessage userMessage = new UserMessage(request.message());
+
         Prompt prompt = new Prompt(systemMessage, userMessage);
 
         return chatClient
